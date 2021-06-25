0aad8f48be66af95a7b8c27156f2609ae1b43acb
patelashok <ashok.patel2917@gmail.com>
2021-06-25 16:34:51 +0530

    for test cases

---
+++ AddressBookServiceTest.java
@@ -0,0 +1,30 @@
+package adressbook;
+
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class AddressBookServiceTest {
+
+    static AddressBookService addressBookService;
+
+    @BeforeClass
+    public static void AddressBookServiceObj() {
+        addressBookService = new AddressBookService();
+    }
+
+    @Test
+    public void givenAddressBookContactsInDB_WhenRetrieved_ShouldMatchContactsCount() throws AddressBookException {
+        List<AddressBookData> addressBookData = addressBookService.readAddressBookData(AddressBookService.IOService.DB_IO);
+        Assertions.assertEquals(3, addressBookData.size());
+    }
+    @Test
+    public void givenNewCityAndStateForContact_WhenUpdated_ShouldMatch() throws AddressBookException {
+        addressBookService.updateContactCityAndState("Sakshi","Pune", "Maharashtra");
+        boolean result = addressBookService.checkAddressBookInSyncWithDB("Sakshi");
+        Assertions.assertTrue(result);
+    }
+}
